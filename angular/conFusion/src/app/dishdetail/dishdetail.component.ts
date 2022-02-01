a597683612d0d812c671ebe8ca540933ade2222b
shivapriya_t <shivapriya.t@gmail.com>
2022-02-01 19:21:40 +0530

    RXJS part2

--- angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,17 +14,30 @@ import { Location } from '@angular/common';
 })
 export class DishdetailComponent implements OnInit {
 
-   
-    dish: Dish;
+
+  dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(private dishservice: DishService,
     private activatedRoute: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit(): void {
-    const id = this.activatedRoute.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe(dish => this.dish=dish);
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    /*const id = this.activatedRoute.snapshot.params['id'];
+    this.dishservice.getDish(id).subscribe(dish => this.dish = dish);*/
+    this.activatedRoute.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
+  
   goBack(): void {
     this.location.back();
   }
